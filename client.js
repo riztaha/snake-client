c43d40c19cf0ff35c0f1ee08bae4c6a0d3339a4a
Taha Rizvi <riztaha@gmail.com>
2020-03-11 18:52:49 +0000

    Added a move up function

--- client.js
+++ client.js
@@ -4,20 +4,26 @@ const connect = function() {
     host: "10.0.2.15",
     port: 50541
   });
+
   conn.setEncoding("utf8");
+
   conn.on("data", data => {
     console.log("Server says: ", data);
   });
   conn.on("connect", () => {
     console.log("Succefully connected to game server.");
-  });
-  conn.on("connect", () => {
+    setInterval(() => {
+    //   conn.write("Move: up");
+    // }, 1000); // - move up one square (unless facing down)
     conn.write("Name: Taha");
   });
 
   return conn;
 };
-// console.log("Connecting...");
-// connect();
 
 module.exports = { connect };
+
+// "Move: up" - move up one square (unless facing down)
+// "Move: down" - move down one square (unless facing up)
+// "Move: left" - move left one square (unless facing right)
+//
