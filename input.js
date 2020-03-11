1df695932a755c40d63b10fa3573aaf510f3d084
Taha Rizvi <riztaha@gmail.com>
2020-03-11 19:44:55 +0000

    Separated the functions in different files (input, play)

---
+++ input.js
@@ -0,0 +1,23 @@
+const setupInput = require("/play");
+
+const handleUserInput = function(key) {
+  //u0003 ctrl+c
+  if (key === "\u0003") {
+    //   console.log("Thanks for using me, ciao!");
+    return process.exit();
+  }
+};
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  //   handleUserInput();
+  stdin.on("data", data => {
+    handleUserInput(data);
+  });
+  return stdin;
+};
+
+module.exports = { setupInput };
--- play.js
+++ play.js
@@ -1,25 +1,7 @@
 const { connect } = require("./client");
+const { setupInput } = require("./input");
+
 console.log("....Connecting.");
+
 connect();
-
-const handleUserInput = function(key) {
-  //u0003 ctrl+c
-  if (key === "\u0003") {
-    //   console.log("Thanks for using me, ciao!");
-    return process.exit();
-  }
-};
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  //   handleUserInput();
-  stdin.on("data", data => {
-    handleUserInput(data);
-  });
-  return stdin;
-};
-
 setupInput();
